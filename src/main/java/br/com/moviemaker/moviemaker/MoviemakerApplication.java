7357c31af2f878d01f97a1fa534385a2c86ecc43
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-03 11:32:53 -0300

    Desserializando e modelando dados da série

--- src/main/java/br/com/moviemaker/moviemaker/MoviemakerApplication.java
+++ src/main/java/br/com/moviemaker/moviemaker/MoviemakerApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.moviemaker.moviemaker.model.DadosSerie;
 import br.com.moviemaker.moviemaker.service.ConsumoApi;
+import br.com.moviemaker.moviemaker.service.ConverteDados;
 
 @SpringBootApplication
 public class MoviemakerApplication implements CommandLineRunner {
@@ -17,6 +19,8 @@ public class MoviemakerApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
