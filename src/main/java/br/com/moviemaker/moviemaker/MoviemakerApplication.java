18c97a042ca0d92c85d5e85015d374630a91bccf
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-04 11:40:22 -0300

    Criando menu de interação

--- src/main/java/br/com/moviemaker/moviemaker/MoviemakerApplication.java
+++ src/main/java/br/com/moviemaker/moviemaker/MoviemakerApplication.java
@@ -1,17 +1,11 @@
 package br.com.moviemaker.moviemaker;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.moviemaker.moviemaker.model.DadosEpisodios;
-import br.com.moviemaker.moviemaker.model.DadosSerie;
-import br.com.moviemaker.moviemaker.model.DadosTemporada;
-import br.com.moviemaker.moviemaker.service.ConsumoApi;
-import br.com.moviemaker.moviemaker.service.ConverteDados;
+import br.com.moviemaker.moviemaker.principal.Principal;
 
 @SpringBootApplication
 public class MoviemakerApplication implements CommandLineRunner {
@@ -22,23 +16,8 @@ public class MoviemakerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
-		DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
-		System.out.println(dadosEpisodios);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 1; i <= dados.totalTemporadas(); i++){
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
-            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibeMenu();
+		
 	}
 }
