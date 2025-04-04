9b882f0df49bdbde09c3352cba11329e8f217986
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-04 11:07:17 -0300

    Modelando episódios

--- src/main/java/br/com/moviemaker/moviemaker/MoviemakerApplication.java
+++ src/main/java/br/com/moviemaker/moviemaker/MoviemakerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.moviemaker.moviemaker.model.DadosEpisodios;
 import br.com.moviemaker.moviemaker.model.DadosSerie;
 import br.com.moviemaker.moviemaker.service.ConsumoApi;
 import br.com.moviemaker.moviemaker.service.ConverteDados;
@@ -22,5 +23,9 @@ public class MoviemakerApplication implements CommandLineRunner {
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+
+		json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
+		DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
+		System.out.println(dadosEpisodios);
 	}
 }
