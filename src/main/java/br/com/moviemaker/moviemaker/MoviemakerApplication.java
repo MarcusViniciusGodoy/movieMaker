0360c7cfd9aaaa138ce5435445a0e6065e7dc71e
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-04 11:15:46 -0300

    Modelando Temporadas

--- src/main/java/br/com/moviemaker/moviemaker/MoviemakerApplication.java
+++ src/main/java/br/com/moviemaker/moviemaker/MoviemakerApplication.java
@@ -1,11 +1,15 @@
 package br.com.moviemaker.moviemaker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.moviemaker.moviemaker.model.DadosEpisodios;
 import br.com.moviemaker.moviemaker.model.DadosSerie;
+import br.com.moviemaker.moviemaker.model.DadosTemporada;
 import br.com.moviemaker.moviemaker.service.ConsumoApi;
 import br.com.moviemaker.moviemaker.service.ConverteDados;
 
@@ -27,5 +31,14 @@ public class MoviemakerApplication implements CommandLineRunner {
 		json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
 		DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
 		System.out.println(dadosEpisodios);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++){
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
