a756945f3c51e1686254d1cd08fa7ebba95ab0fa
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-04 11:49:45 -0300

    Coleção de dados

--- src/main/java/br/com/moviemaker/moviemaker/principal/Principal.java
+++ src/main/java/br/com/moviemaker/moviemaker/principal/Principal.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import br.com.moviemaker.moviemaker.model.DadosEpisodios;
 import br.com.moviemaker.moviemaker.model.DadosSerie;
 import br.com.moviemaker.moviemaker.model.DadosTemporada;
 import br.com.moviemaker.moviemaker.service.ConsumoApi;
@@ -27,11 +28,14 @@ public class Principal {
 
         List<DadosTemporada> temporadas = new ArrayList<>();
 
-		for(int i = 1; i<=dados.totalTemporadas(); i++) {
+		for(int i = 1; i<= dados.totalTemporadas(); i++) {
             json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season=" + i + API_KEY);  
             DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
             temporadas.add(dadosTemporada);
         }
-        temporadas.forEach(System.out::println);
+        //temporadas.forEach(System.out::println);
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        //temporadas.forEach(System.out::println);
     }
 }
