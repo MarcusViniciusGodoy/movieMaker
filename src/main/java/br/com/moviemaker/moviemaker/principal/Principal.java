97cd5d3cc2accf2b24282ea04a645c957877aa03
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-06 11:27:03 -0300

    Identificando top 5 episódios

--- src/main/java/br/com/moviemaker/moviemaker/principal/Principal.java
+++ src/main/java/br/com/moviemaker/moviemaker/principal/Principal.java
@@ -1,8 +1,12 @@
 package br.com.moviemaker.moviemaker.principal;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import br.com.moviemaker.moviemaker.model.DadosEpisodios;
 import br.com.moviemaker.moviemaker.model.DadosSerie;
@@ -33,9 +37,17 @@ public class Principal {
             DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
             temporadas.add(dadosTemporada);
         }
-        //temporadas.forEach(System.out::println);
 
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
-        //temporadas.forEach(System.out::println);
+        
+        List<DadosEpisodios> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+        System.out.println("\nTop 5 episódios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodios::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
