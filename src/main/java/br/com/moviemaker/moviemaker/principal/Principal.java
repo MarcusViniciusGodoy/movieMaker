ceca2ce27b5d06918085bac468d2a683cd9df393
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-11 11:07:18 -0300

    Busca séries por categorias

--- src/main/java/br/com/moviemaker/moviemaker/principal/Principal.java
+++ src/main/java/br/com/moviemaker/moviemaker/principal/Principal.java
@@ -13,6 +13,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import br.com.moviemaker.moviemaker.model.Categoria;
 import br.com.moviemaker.moviemaker.model.DadosEpisodios;
 import br.com.moviemaker.moviemaker.model.DadosSerie;
 import br.com.moviemaker.moviemaker.model.DadosTemporada;
@@ -48,9 +49,10 @@ public class Principal {
                 1 - Buscar séries
                 2 - Buscar episódios
                 3 - Listar séries buscadas
-                4 - Buscar série por titulo
-                5 - Buscar série por ator
+                4 - Buscar séries por titulo
+                5 - Buscar séries por ator
                 6 - Top 5 séries
+                7 - Busca séries por categoria
 
                 0 - Sair
                 """;
@@ -77,6 +79,9 @@ public class Principal {
                         break;
                     case 6:
                         buscarTop5Series();
+                        break;
+                    case 7:
+                        buscarSeriesPorCategoria();
                         break;
                     case 0:
                         System.out.println("Saindo...");
@@ -169,6 +174,16 @@ public class Principal {
         private void buscarTop5Series(){
             List<Serie> serieTop = repository.findTop5ByOrderByAvaliacaoDesc(); 
             serieTop.forEach(s ->
+            System.out.println(s.getTitulo() + " avaliação: " + s.getAvaliacao()));
+        }
+
+        private void buscarSeriesPorCategoria(){
+            System.out.println("Deseja buscar séries de que categoria/gênero?");
+            var nomeGenero = leitura.nextLine();
+            Categoria categoria = Categoria.fromPortugues(nomeGenero);
+            List<Serie> seriesPorCategoria = repository.findByGenero(categoria);
+            System.out.println("Séries da categoria " + nomeGenero);
+            seriesPorCategoria.forEach(s -> 
             System.out.println(s.getTitulo() + " avaliação: " + s.getAvaliacao()));
         }
 }
