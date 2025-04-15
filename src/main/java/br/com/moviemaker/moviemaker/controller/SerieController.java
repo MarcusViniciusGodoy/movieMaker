2e88eb26e7cd05d9e8544adea6277ef6a7d4a444
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-15 11:30:51 -0300

    Representando séries do banco

--- src/main/java/br/com/moviemaker/moviemaker/controller/SerieController.java
+++ src/main/java/br/com/moviemaker/moviemaker/controller/SerieController.java
@@ -1,13 +1,28 @@
 package br.com.moviemaker.moviemaker.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.moviemaker.moviemaker.dto.SerieDTO;
+import br.com.moviemaker.moviemaker.model.Serie;
+import br.com.moviemaker.moviemaker.repository.SerieRepository;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repository;
+
     @GetMapping("/series")
-    public String obterSeries(){
-        return "Aqui vão ser listadas as séries";
+    public List<SerieDTO> obterSeries(){
+        return repository.findAll()
+        .stream().map(s -> new SerieDTO(s.getId(),
+        s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(),
+        s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+        .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/br/com/moviemaker/moviemaker/dto/SerieDTO.java
@@ -0,0 +1,14 @@
+package br.com.moviemaker.moviemaker.dto;
+
+import br.com.moviemaker.moviemaker.model.Categoria;
+
+public record SerieDTO(Long id,
+                       String titulo,
+                       Integer totalTemporadas,
+                       Double avaliacao,
+                       Categoria genero,
+                       String atores,
+                       String poster,
+                       String sinopse) {
+
+}
