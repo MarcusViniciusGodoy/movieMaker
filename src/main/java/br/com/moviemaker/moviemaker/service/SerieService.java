4e768c59d9451cf6c136a73e6be029acbdcd1d1a
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-16 10:27:39 -0300

    Busca séries por data de lançamento

--- src/main/java/br/com/moviemaker/moviemaker/service/SerieService.java
+++ src/main/java/br/com/moviemaker/moviemaker/service/SerieService.java
@@ -24,6 +24,10 @@ public class SerieService {
         return converteDados(repository.findTop5ByOrderByAvaliacaoDesc());
     }
 
+    public List<SerieDTO> obterLancamentos() {
+        return converteDados(repository.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+    }
+
     private List<SerieDTO> converteDados(List<Serie> series){
         return series.stream().map(s -> new SerieDTO(s.getId(),
             s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(),
